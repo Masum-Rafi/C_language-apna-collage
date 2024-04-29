1337ed8d54267796c50fa2febbd68145885abcec
Masum-Rafi <masumbillahrafi0@gmail.com>
2024-04-30 01:14:21 +0600

    input-output operation

---
+++ Chapter-1/Input-output.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+   int a, b;
+
+   printf("Enter a number : \n");
+   scanf("%d", &a); // Taiking input for variable a
+
+   printf("Enter a number : \n");
+   scanf("%d", &b); // Taiking input for variable a
+
+   printf("The sum of This numbers : %d", a + b); // Printing output sum of n numbers
+   return 0;
+}
--- Chapter-1/Q2.C
+++ Chapter-1/Q2.C
@@ -1,11 +1,12 @@
+// Take a number(n) from user & output its cube(n*n*n).
 #include<stdio.h>
 int main(){
-    int a;
+    int a; // dicler a variable called a
 
-    printf("Enter A number :\n ");
-    scanf("%d", &a);
+    printf("Enter A number :\n "); // Printing a message for take input
+    scanf("%d", &a);               // take input from user
 
-    printf("The Cube of Number is %d", a*a*a);
+    printf("The Cube of Number is %d", a * a * a); // calculate and Print cube of n
 
     return 0;
 }
