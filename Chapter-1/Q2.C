02e3330c9cc49011d820de7a6c6c46aee650586e
Masum-Rafi <masumbillahrafi0@gmail.com>
2024-04-29 19:51:07 +0600

    Q2 : Complete the problem

--- Chapter-1/Q2.C
+++ Chapter-1/Q2.C
@@ -3,6 +3,9 @@ int main(){
     int a;
 
     printf("Enter A number :/n ");
-    scanf("%d", &a, &b);
+    scanf("%d", &a);
+
+    printf("The Cube of Number is %d, a*a*a");
+    
     return 0;
 }
