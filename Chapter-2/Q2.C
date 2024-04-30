0bf45aa4b582e65fc3bd83b3526245650ebd9e13
Masum-Rafi <masumbillahrafi0@gmail.com>
2024-04-30 16:53:44 +0600

    .\Program for finding smallest number of two

--- Chapter-2/Q2.C
+++ Chapter-2/Q2.C
@@ -1,6 +1,18 @@
+// Write a program to print the smallest number of two.
+
 #include <stdio.h>
 int main()
 {
+   int a, b;
+
+   printf("Enter a : \n");
+   scanf("%d", &a);
+
+   printf("Enter b : \n");
+   scanf("%d", &b);
+
+   (a > b) ? printf("Smallest number is : %d", b) : printf("Smallest Number is : %d", a);
+   // in this program i used the Ternary Oparator. its a short from if/else condition
 
    return 0;
 }
