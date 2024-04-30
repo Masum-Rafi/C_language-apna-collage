316d3faa4bed7e1922c3602516788cb93bc8524c
Masum-Rafi <masumbillahrafi0@gmail.com>
2024-04-30 12:27:33 +0600

    .\ArithamaticOparatioC

---
+++ Chapter-2/ArithamaticOparation.C
@@ -0,0 +1,15 @@
+// Arithamatix Instruction Oparation
+// Note : Single variable on LHS
+
+#include <stdio.h>
+int main()
+{
+   int a = 5, b = 10;
+   int sum = a + b;
+   int multiply = a * b; // single variable on LHS
+
+   printf("%d\n", sum);
+   printf("%d\n", multiply);
+
+   return 0;
+}
