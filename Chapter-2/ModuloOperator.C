0f1f181e28d5a1722a3f81731d5a3145f4111db2
Masum-Rafi <masumbillahrafi0@gmail.com>
2024-05-01 16:34:50 +0600

    ModuloOperator

---
+++ Chapter-2/ModuloOperator.C
@@ -0,0 +1,16 @@
+// Modulo/Moduler Operator
+
+#include <stdio.h>
+
+int main()
+{
+   int a, b;
+
+   printf("Enter Number a : \n");
+   scanf("%d", &a);
+
+   printf("Enter Number b : \n");
+   scanf("%d", &b);
+
+   printf("%d", (a % b));
+}
