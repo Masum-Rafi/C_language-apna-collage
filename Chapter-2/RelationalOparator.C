1e31aa94e4d6cc41da357516c355faf025878dd2
Masum-Rafi <masumbillahrafi0@gmail.com>
2024-05-04 20:56:07 +0600

    Relational oparator

---
+++ Chapter-2/RelationalOparator.C
@@ -0,0 +1,17 @@
+// Relational Oparator
+// If ans is 1 its true if its 0 then its false
+
+#include <stdio.h>
+
+int main()
+{
+   printf("%d\n", 2 == 2);
+   printf("%d\n", 3 == 2);
+   printf("%d\n", 4 > 2);
+   printf("%d\n", 2 >= 2);
+   printf("%d\n", 2 < 2);
+   printf("%d\n", 2 < 5);
+   printf("%d\n", 6 != 2);
+
+   return 0;
+}
