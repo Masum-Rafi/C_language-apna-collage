0abf6904bd30d788c2bfc75704085567e1440eca
Masum-Rafi <masumbillahrafi0@gmail.com>
2024-05-14 07:04:12 +0600

    renamed

--- Chapter-2/Q2.C
+++ Chapter-2/Q2.C
@@ -8,6 +8,6 @@ int main()
    printf("Enter a character : ");
    scanf("%c", &a);
 
-   (a >= '0' && a <= '9') ? printf("%c Is a Digit", a) : printf("%c is a Character");
+   (a >= '0' && a <= '9') ? printf("%c Is a Digit", a) : printf("%c is a Character"); // in this program i used the Ternary Oparator. its a short from if/else condition
    return 0;
 }
