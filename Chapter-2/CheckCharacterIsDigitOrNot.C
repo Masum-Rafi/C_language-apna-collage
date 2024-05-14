ae79de705631e47e2c7a8a8b058144d7423ff00e
Masum-Rafi <masumbillahrafi0@gmail.com>
2024-05-14 07:03:20 +0600

    Check Character Is Digit Or Not & Renamed Q2,Q3

---
+++ Chapter-2/CheckCharacterIsDigitOrNot.C
@@ -0,0 +1,13 @@
+// A program to check if given character is digit or not.
+
+#include <stdio.h>
+int main()
+{
+
+   char a;
+   printf("Enter a character : ");
+   scanf("%c", &a);
+
+   (a >= '0' && a <= '9') ? printf("%c Is a Digit", a) : printf("%c is a Character");
+   return 0;
+}
