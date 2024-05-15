1da47f7043389f5c12a037005fd1474d1e2de6c2
Masum-Rafi <masumbillahrafi0@gmail.com>
2024-05-15 08:11:20 +0600

    Ternary Operator

---
+++ Chapter-3/TernaryOperator.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+   int Age;
+   printf("Enter Age : ");
+   scanf("%d", &Age);
+
+   (Age >= 18) ? printf("He/She is Adult") : printf("Not Adult \nvag ehasy BSDK");
+
+   return 0;
+}
