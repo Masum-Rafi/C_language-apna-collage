995b4b090a9d7ccf074501c05efac7c5b7a55d5a
Masum-Rafi <masumbillahrafi0@gmail.com>
2024-05-15 06:45:11 +0600

    Update ch-2>Q1

--- Chapter-2/Q1.C
+++ Chapter-2/Q1.C
@@ -7,5 +7,12 @@ int main()
    scanf("%d %d %d", &a, &b, &c);
 
    printf("Average of three numbers : %.2f \n", (a + b + c) / 3.0);
+   /*%.2f is a format specifier. Here's what it means:
+   >> % indicates the start of a format specifier.
+   >> .2 specifies that the number should be printed with 2 digits after the decimal point.
+   >> f specifies that the number is a floating-point value*/
+
+   // And also we need to use a float value for devide the sum aither we will get 0.00 for our answer
+
    return 0;
 }
