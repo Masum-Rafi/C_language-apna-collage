302367a7d4d2bae32949431746781b4088758024
Masum-Rafi <masumbillahrafi0@gmail.com>
2024-05-29 13:11:13 +0600

    infiniteLoop

---
+++ Chapter-4/infiniteLoop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+   for (int i = 0;; i)
+      ;
+   printf("Hello World");
+   return 0;
+}
+
+// We need to very careful while writing the loop, because if we forget to write the condition in the loop, then it will become an infinite loop. this program will run forever and will not stop until we stop it manually or it will fill our memory and crash the system.
